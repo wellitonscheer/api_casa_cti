608fd6304ef9b741de53fd7501dea2e1630fc978
wellitonscheer <wellitonscheer@gmail.com>
2022-06-13 18:43:44 -0300

    link do ngrok

--- Services/HelperService.cs
+++ Services/HelperService.cs
@@ -14,7 +14,9 @@ namespace api1.Services
     public class HelperService
     {
         //private NpgsqlConnection connection;
-        private const string BASE_URL_ARDUINO = "http://192.168.2.74/?acao=";
+        //private const string BASE_URL_ARDUINO = "http://192.168.21.153:90/?acao=";
+        private const string BASE_URL_ARDUINO = "https://af71-2804-18-3f-6a88-f384-1d94-3ac9-d738.sa.ngrok.io/?acao=";
+        
         NpgsqlConnection connection = new NpgsqlConnection("Server=ec2-52-204-195-41.compute-1.amazonaws.com;Port=5432;User Id=ptiupmxqefzbrw;Password=b0d2608030cee865d43b71bd99af5856c2bc1f7bd3d7e38e118288d90cfa2077;Database=dem3st7tq31kr7;");
 
         public async Task<T> RequestArduino<T>(string path)
