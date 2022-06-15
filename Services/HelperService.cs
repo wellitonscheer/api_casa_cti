2f9aa77f888bc0e6828c0edb8ecc000d96f03616
wellitonscheer <wellitonscheer@gmail.com>
2022-06-15 09:48:09 -0300

    link

--- Services/HelperService.cs
+++ Services/HelperService.cs
@@ -15,7 +15,7 @@ namespace api1.Services
     {
         //private NpgsqlConnection connection;
         //private const string BASE_URL_ARDUINO = "http://192.168.21.153:90/?acao=";
-        private const string BASE_URL_ARDUINO = "https://ad9b-2804-18-1096-734c-a37d-8a15-a97d-ce7f.sa.ngrok.io/?acao=";
+        private const string BASE_URL_ARDUINO = "https://52b0-143-208-185-148.sa.ngrok.io/?acao=";
         
         NpgsqlConnection connection = new NpgsqlConnection("Server=ec2-52-204-195-41.compute-1.amazonaws.com;Port=5432;User Id=ptiupmxqefzbrw;Password=b0d2608030cee865d43b71bd99af5856c2bc1f7bd3d7e38e118288d90cfa2077;Database=dem3st7tq31kr7;");
 
