734c12a9a763abcbfd53f2d7776052c10273f066
wellitonscheer <wellitonscheer@gmail.com>
2022-10-24 19:37:57 -0300

    ngrok

--- Services/HelperService.cs
+++ Services/HelperService.cs
@@ -15,7 +15,7 @@ namespace api1.Services
     {
         //private NpgsqlConnection connection;
         //private const string BASE_URL_ARDUINO = "http://192.168.21.153/?acao=";
-        private const string BASE_URL_ARDUINO = "https://cac2-187-109-228-129.sa.ngrok.io/?acao=";
+        private const string BASE_URL_ARDUINO = "https://2c56-138-94-78-86.sa.ngrok.io/?acao=";
         
         IDbConnection connection = ConnectionService.GetInstance().connection;
 
